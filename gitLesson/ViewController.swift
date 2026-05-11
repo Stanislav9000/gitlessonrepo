a1ee2c451fa730e84a77809ad0427a4bdb5bec9f
Станислав <Stanislav9000@bk.ru>
2026-05-11 22:15:43 +0500

    blank project created

---
+++ gitLesson/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  gitLesson
+//
+//  Created by Тест on 11.05.2026.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
+
