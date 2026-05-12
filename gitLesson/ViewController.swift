9c1485f8a85c1cd2e962443f067bd08e17fed4e2
Станислав <Stanislav9000@bk.ru>
2026-05-12 22:04:33 +0500

    hello printed

--- gitLesson/ViewController.swift
+++ gitLesson/ViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final  class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		print("hello")
 	}
 }
 
